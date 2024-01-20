f940b112e5e06499c10b9b7f13ea02d70422e672
Harshith <hkukunuru@gmail.com>
2024-01-20 13:21:03 -0500

    Reordering.
    Put the welcome to chatbot in main instead of chat.

--- website/components/chatbot.jsx
+++ website/components/chatbot.jsx
@@ -26,39 +26,8 @@ const Chatbot = () => {
 
   return (
     <>
-      <div className="container mx-auto flex items-center justify-center pt-10">
-        <h1 className="text-4xl text-gray-800 font-bold tracking-wider font-serif">
-          Talk to AI
-        </h1>
-      </div>
-      <div className="grid grid-cols-2 grid-flow-col gap-4">
-        <div className="grid grid-rows-5 gap-4 p-10">
-          <div className=" justify-center row-span-3 pl-10">
-            <p className="text-2xl text-gray-800 font-serif tracking-wider leading-10 p-3">
-              Explore the world of finance with our new chat feature, designed
-              to make discussing financial matters feel like a natural
-              conversation with a trusted friend. Need advice on investments or
-              want to know more about current market trends? Just chat with our
-              AI-driven financial assistant, and we'll guide you through it in a
-              way that's easy to understand.
-            </p>
-          </div>
-          <div className="flex items-center justify-center row-span-1">
-            <button className="bg-gray-800 text-gray-200 hover:text-white font-bold py-2 px-4 rounded-full">
-              <p className="text-2xl text-white-800 font-serif tracking-wider leading-10 p-1">
-                Jump to chat
-              </p>
-            </button>
-          </div>
-        </div>
-
-        <div className="flex justify-center">
-          <img className="w-3/4" src="/Chatbot.png"></img>
-        </div>
-      </div>
-
-      <div className="flex flex-col h-screen bg-gray-100">
-        <div className="flex flex-col flex-grow overflow-y-auto p-8">
+      <div className="flex flex-col h-screen p-5">
+        <div className="flex flex-col flex-grow bg-gray-500 overflow-y-auto p-8">
           {messages.map((message, index) => (
             <div
               key={index}
--- website/components/intro.jsx
+++ website/components/intro.jsx
@@ -2,17 +2,49 @@ import React from "react";
 
 export default function Intro() {
   return (
-    <div className="bg-gray-200 text-gray-800 p-8">
-      <h1 className="text-3xl font-bold font-serif text-center mb-4">
-        Welcome to The Third Street Journal!
-      </h1>
-      <p className="text-center">
-        This is your one-stop shop for financial news! This application
-        extensively scrapes the internet for the latest financial news across
-        the most reputable sources of news in the world. Through thorough AI
-        processing, we developed a chat feature to look into any information
-        that any analyst may want to research.
-      </p>
-    </div>
+    <>
+      <div className="bg-gray-200 text-gray-800 p-8">
+        <h1 className="text-4xl text-gray-800 font-bold tracking-wider font-serif text-center mb-4">
+          Welcome to The Third Street Journal!
+        </h1>
+        <p className="text-center text-2xl text-gray-800 font-serif tracking-wider leading-10 p-3">
+          This is your one-stop shop for financial news! This application
+          extensively scrapes the internet for the latest financial news across
+          the most reputable sources of news in the world. Through thorough AI
+          processing, we developed a chat feature to look into any information
+          that any analyst may want to research.
+        </p>
+      </div>
+      <div className="container mx-auto flex items-center justify-center pt-10">
+        <h1 className="text-4xl text-gray-800 font-bold tracking-wider font-serif">
+          Talk to AI
+        </h1>
+      </div>
+      <div className="grid grid-cols-2 grid-flow-col gap-4">
+        <div className="grid grid-rows-5 gap-4 p-10">
+          <div className=" justify-center row-span-3 pl-10">
+            <p className="text-2xl text-gray-800 font-serif tracking-wider leading-10 p-3">
+              Explore the world of finance with our new chat feature, designed
+              to make discussing financial matters feel like a natural
+              conversation with a trusted friend. Need advice on investments or
+              want to know more about current market trends? Just chat with our
+              AI-driven financial assistant, and we'll guide you through it in a
+              way that's easy to understand.
+            </p>
+          </div>
+          <div className="flex items-center justify-center row-span-1">
+            <button className="bg-gray-800 text-gray-200 hover:text-white font-bold py-2 px-4 rounded-full">
+              <p className="text-2xl text-white-800 font-serif tracking-wider leading-10 p-1">
+                Jump to chat
+              </p>
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <img className="w-3/4" src="/Chatbot.png"></img>
+        </div>
+      </div>
+    </>
   );
 }
