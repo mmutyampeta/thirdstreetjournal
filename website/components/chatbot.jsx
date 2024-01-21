21ab45e73ef0ebd58426e0f1eb41a803b0a5f2ae
Harshith <hkukunuru@gmail.com>
2024-01-20 19:59:47 -0500

    centering button + trading view

--- website/components/chatbot.jsx
+++ website/components/chatbot.jsx
@@ -35,12 +35,18 @@ const Chatbot = () => {
         {renderChatBox ? (
           <Box />
         ) : (
-          <button
-            onClick={chatSetup}
-            className="bg-gray-800 text-white hover:text-gray-900 font-bold py-2 px-4 rounded-full"
-          >
-            Start Chat!
-          </button>
+          <>
+            <div className="flex items-center justify-center p-10">
+              <button
+                className="bg-gray-800 text-white hover:text-gray-800 hover:bg-white hover:border hover:border-gray-800 font-bold py-2 px-4 rounded-full"
+                onClick={chatSetup}
+              >
+                <p className="text-2xl text-white-800 font-serif tracking-wider leading-10 p-1">
+                  Start Chat!
+                </p>
+              </button>
+            </div>
+          </>
         )}
       </div>
     </>
--- website/pages/_app.js
+++ website/pages/_app.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Header from "../components/header";
 import Intro from "../components/intro";
 import Chatbot from "../components/chatbot";
+import TradingViewWidget from "../components/chart";
 
 function MyApp() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -11,7 +12,7 @@ function MyApp() {
       <Header setSelectedPage={setSelectedPage} />
       {selectedPage === "home" && <Intro setSelectedPage={setSelectedPage} />}
       {selectedPage === "chat" && <Chatbot />}
-      {selectedPage === "stocks"}
+      {selectedPage === "stocks" && <TradingViewWidget />}
     </>
   );
 }
